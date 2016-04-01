b36508d09075147249b6887db31ad952b725f63e
karlaking <karlaking2@gmail.com>
2016-04-01 11:36:41 -0400

    remove rendering actual images

--- rock_reconstruction.py
+++ rock_reconstruction.py
@@ -1,16 +1,18 @@
 # Place the images correctly to create a mosaic.
 
 
-# Parse csv into geojson file including attributes: yaw, pitch, roll, x, y, z, and image location(nil upon initialization)
-# Upload images to web host, such as imgur(may not be necessary for this first step)
-  # Add file web locations to geojson
+# Parse csv into geojson file including attributes: yaw, pitch, roll, x, y, z, and image location
 
 # Create model with image objects
 # Seed database with each image object
 
 # Write function to geo-reference images
   # put images into matrix based on x.y values
-  # Do not know how to use the z values yet
 
-# Render the files visually on a web browser
+
+
+
+
+
+
 
