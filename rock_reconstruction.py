befe013456a74c6583870ee657fd7fbfbaa660b0
karlaking <karlaking2@gmail.com>
2016-04-10 14:58:56 -0700

    Begin image class

--- rock_reconstruction.py
+++ rock_reconstruction.py
@@ -8,6 +8,20 @@
 
 # Write function to geo-reference images
   # put images into matrix based on x.y values
+  # find max long, min long, max lat, min lat
+
+class Image:
+  def _init_(self, lat, long, alt, yaw, pitch, roll)
+      self.lat = lat
+      self.long = long
+      self.alt = alt
+      self.yaw = yaw
+      self.pitch = pitch
+      self.roll = roll
+
+
+
+
 
 
 
